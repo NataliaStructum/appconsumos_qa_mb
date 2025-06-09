a1f0d57bb6407125906890d562455e04e345ce7a
Sarah Cano <scanob@incauca.com>
2025-06-09 14:38:49 +0000

    listado ordenesingenio 2

--- Rules/Ingenio/get_List_Ingenio_Equipo.js
+++ Rules/Ingenio/get_List_Ingenio_Equipo.js
@@ -19,7 +19,7 @@ export default function get_List_Ingenio_Equipo(context) {
         filtro += `equipo eq '${e.BindingObject.equipo}' or`      
     });
 
-    return context.read('/appconsumos_qa_mb/Services/app_consumos_qa.service', 'Ordenes', [], filtro.slice(0, -3)).then(async (results) => {
+    return context.read('/appconsumos_qa_mb/Services/app_consumos_qa.service', 'Ordenes', [], filtro.slice(0, -3)+"&$orderby=fecha_creacion desc").then(async (results) => {
         if (results && results.length > 0) {
             alert(results.length)
             clientData.lista_ingenio = results
