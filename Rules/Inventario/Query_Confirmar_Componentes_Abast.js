b05918144210ff5618f2bbb8664b1b32b3a21647
Natalia Lopez Osorio <natalia.lopez@structum-co.com>
2025-07-23 22:02:43 +0000

    correcion unidades de medida parte 2

--- Rules/Inventario/Query_Confirmar_Componentes_Abast.js
+++ Rules/Inventario/Query_Confirmar_Componentes_Abast.js
@@ -4,7 +4,7 @@
  */
 export default function Query_Confirmar_Componentes_Abast(context) {
     let id_solicitud = context.binding.id
-    let filtro = `$expand=material,almacen&$filter=solicitud_id eq ${id_solicitud} and aprobado eq true`
+    let filtro = `$expand=material,material/und,almacen&$filter=solicitud_id eq ${id_solicitud} and aprobado eq true`
 
     return filtro
 }
